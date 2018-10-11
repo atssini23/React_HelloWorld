f4c27cd34a1f81abc1531f6f7a6b2022491e45c5
Atssini Cabrera <atssinicabrera@Atssinis-MacBook-Pro.local>
2018-10-10 21:38:41 -0700

    state n prop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import Atssini from './Atssini';
-import './App.css';
+import Atssini from './Atssini'; {/* without this app will break, react wont find the component */}
 
 class App extends Component {
+  constructor() {
+     super(); {/* must call */}
+     this.state = {
+       greeting: "Hello World!",
+       parentMessage: "Hello Atssini!"
+     }
+  }
+
   render() {
     return(
-      <div>
-        <h1>Hello World!</h1>
-        <Atssini/>
+      <div className="greeting"> 
+        <h1>{this.state.greeting}</h1> {/* the way of evaluating JS expressions within JSX */}
+        <Atssini newMsg={this.state.parentMessage}/> {/* child component */}
       </div>
     );
   }
--- src/Atssini.js
+++ src/Atssini.js
@@ -1,10 +1,16 @@
 import React,  { Component } from 'react';
 
 class Atssini extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            greeting: props.newMsg
+        }
+    }
     render () {
         return (
             <div>
-                <h2>Hi Atssini here</h2>
+                <h2>{this.state.greeting}</h2>
             </div>
         )
     }
