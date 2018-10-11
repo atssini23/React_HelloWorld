b5b11d41691fa61dbd6dabdef3d7b233c030d6c3
Atssini Cabrera <atssinicabrera@Atssinis-MacBook-Pro.local>
2018-10-10 21:20:36 -0700

    hello atssini here

---
+++ src/Atssini.js
@@ -0,0 +1,13 @@
+import React,  { Component } from 'react';
+
+class Atssini extends Component {
+    render () {
+        return (
+            <div>
+                <h2>Hi Atssini here</h2>
+            </div>
+        )
+    }
+}
+
+export default Atssini;
